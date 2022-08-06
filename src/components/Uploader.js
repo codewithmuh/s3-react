9598068af1855bef83e0190715ec1bdfb714d70a
Muhammad Rashid <rashiddaha80@gmail.com>
2022-08-07 03:56:51 +0500

    new updates

--- src/components/Uploader.js
+++ src/components/Uploader.js
@@ -1,27 +1,52 @@
-import Dropzone from 'react-dropzone-uploader'
-import 'react-dropzone-uploader/dist/styles.css'
-
-import React from 'react';
+import React from "react";
+import "react-dropzone-uploader/dist/styles.css";
+import Dropzone from "react-dropzone-uploader";
 
 const Uploader = () => {
-    
-    
-    const handleChangeStatus = ({ meta, remove }, status) => {
-       console.log(status, meta);
-    }
-    return (
-        <Dropzone
-          onChangeStatus={handleChangeStatus}
-          maxFiles={2}
-          multiple={false}
-          canCancel={false}
-          inputContent="Drop A File"
-          styles={{
-            dropzone: { width: 400, height: 200 },
-            dropzoneActive: { borderColor: 'green' },
-          }}
-        />
-    )
-  }
-  <Uploader />
-export default Uploader
+  const axios = require("axios").default;
+
+  const API_ENDPOINT =
+    "https://r1h6co0u6l.execute-api.us-west-2.amazonaws.com/getPresignedImageURL";
+  const handleChangeStatus = ({ meta, remove }, status) => {
+    console.log(status, meta);
+  };
+
+  const handleSubmit = async (files) => {
+    const f = files[0];
+    console.log(f["file"]);
+    // * GET request: presigned URL
+    const response = await axios({
+      method: "GET",
+      url: API_ENDPOINT,
+    });
+
+    console.log("Response: ", response);
+
+    // * PUT request: upload file to S3
+    const result = await fetch(response.data.uploadURL, {
+      method: "PUT",
+      body: f["file"],
+    });
+    console.log("Result: ", result);
+  };
+
+  return (
+    <Dropzone
+      onChangeStatus={handleChangeStatus}
+      onSubmit={handleSubmit}
+      hjello
+      maxFiles={1}
+      multiple={false}
+      canCancel={false}
+      inputContent="Drop A File"
+      styles={{
+        dropzone: { width: 400, height: 200 },
+        dropzoneActive: { borderColor: "green" },
+      }}
+    />
+  );
+};
+
+<Uploader />;
+
+export default Uploader;
